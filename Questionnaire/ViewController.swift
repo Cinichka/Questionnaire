9965d154f3b5bd9ee45ae6f538ec643067f8824f
cinichka85 <cinichka85@gmail.com>
2018-09-06 20:35:50 +0300

    Initial Commit

---
+++ Questionnaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Questionnaire
+//
+//  Created by Вероника Садовская on 06.09.2018.
+//  Copyright © 2018 Veronika Sadovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
